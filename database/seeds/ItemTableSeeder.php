3b263d3687dfa72653af5090e2efef97d6b5a256
大倉祥一 <okurashoichi@team-lab.com>
2019-02-12 10:59:50 +0900

    no message

---
+++ database/seeds/ItemTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ItemTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('items')->insert([
+            'name' => str_random(10),
+            'description' => str_random(10),
+            'price' => rand ( 100 , 9999 ),
+            'image' => str_random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
