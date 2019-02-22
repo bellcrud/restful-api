2e2668a0d7d6a507a3d47822f748281a248a9e00
大倉祥一 <okurashoichi@team-lab.com>
2019-02-22 18:49:41 +0900

    [update]権限がない場合遷移先画面修正

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,6 +1,8 @@
-<div>
-    <div>
-        <h2>ログインしてから再度アクセスし直してください</h2>
-        <a href="/login/github">Log in with Github</a>
-    </div>
+<div class="jumbotron">
+    <h1 class="display-3">{{$status_code or 'error'}}</h1>
+    <p class="lead">{{ $message or 'Error'}}</p>
+    <hr class="my-4">
+    <p class="lead">
+        <a class="btn btn-primary" href="/" role="button">TOP</a>
+    </p>
 </div>
