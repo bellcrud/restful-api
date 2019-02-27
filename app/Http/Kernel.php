d386f957091e7704b0fec8ce178ad0e2a287ddce
大倉祥一 <okurashoichi@team-lab.com>
2019-02-27 18:22:51 +0900

    [update]TokenチェックMiddleware追加
    Tokenの有無と有効期限をチェックするMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'ajax' => \App\Http\Middleware\AjaxOnlyMiddleware::class
+        'ajax' => \App\Http\Middleware\AjaxOnlyMiddleware::class,
+		'token' => \App\Http\Middleware\TokenCheck::class
     ];
 }
