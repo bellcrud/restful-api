3041b0079ab56020780a17bd28bf7e1912c93bc3
大倉祥一 <okurashoichi@team-lab.com>
2019-03-03 16:56:22 +0900

    [update]不要な変数格納記述削除

--- app/Http/Middleware/TokenCheck.php
+++ app/Http/Middleware/TokenCheck.php
@@ -8,27 +8,26 @@ use Closure;
 
 class TokenCheck
 {
-	/**
-	 * Handle an incoming request.
-	 * トークンチェック
-	 * ヘッダーに組み込まれたトークンを検索し、Exception\Handlerに渡す
-	 * @param  \Illuminate\Http\Request $request
-	 * @param  \Closure $next
-	 * @return mixed
-	 * @throws TokenException
-	 */
+    /**
+     * Handle an incoming request.
+     * トークンチェック
+     * ヘッダーに組み込まれたトークンを検索し、Exception\Handlerに渡す
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     * @throws TokenException
+     */
     public function handle($request, Closure $next)
     {
-		//ヘッダーに認証用トークンが含まれているか確認
-		if (!$request->header('Authorization')){
-			throw new TokenException();
+        //ヘッダーに認証用トークンが含まれているか確認
+        if (!$request->header('Authorization')) {
+            throw new TokenException();
 
-			////トークンがTokensテーブルに存在しかつ有効期限が過ぎていないかチェック
-		}elseif (!empty($token = Token::findByToken($request->header('Authorization'))) && !Token::calculationPastDay($token->created_at))
-		{
-			return $next($request);
-		}else{
-			throw new TokenException();
-		}
+            ////トークンがTokensテーブルに存在しかつ有効期限が過ぎていないかチェック
+        } elseif (!empty(Token::findByToken($request->header('Authorization'))) && !Token::calculationPastDay(Token::findByToken($request->header('Authorization'))->created_at)) {
+            return $next($request);
+        } else {
+            throw new TokenException();
+        }
     }
 }
