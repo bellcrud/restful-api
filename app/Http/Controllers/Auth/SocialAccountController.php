b231841c7d3aa67c697c004cd913e3f181e1348b
okurashoichi <okurashoichi@team-lab.com>
2019-06-06 20:30:11 +0900

    [update]不要なクエリストリングを削除

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -75,6 +75,8 @@ class SocialAccountController extends Controller
 
         //Cokkieにトークンを設定
         $cookie = Cookie::make('TOKEN', $token->token, config('cookie.tokenDeadline'), null, null, null, false);
+        //不要なCookie情報を削除
+        $cookie = $cookie->getName() . '=' . $cookie->getValue().';';
 
         //SPAにリダイレクト
         return redirect(env('REACT_APP_HOST_NAME') . '?' . $cookie);
