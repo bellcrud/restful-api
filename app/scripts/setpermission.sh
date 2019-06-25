1d5f4ea3afd87c91675998f41d1d9cd008f569f7
okurashoichi <okurashoichi@team-lab.com>
2019-06-25 21:12:55 +0900

    no message

--- app/scripts/setpermission.sh
+++ app/scripts/setpermission.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/html/okura-restful-api
 chmod -R 777 /var/www/html/okura-restful-api
-composer update -d /var/www/html/okura-restful-api
+composer install -d /var/www/html/okura-restful-api
