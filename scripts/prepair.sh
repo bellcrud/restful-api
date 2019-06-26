ad7adb2952d77366ef79adacfc682f091f812ad5
okurashoichi <okurashoichi@team-lab.com>
2019-06-26 23:57:39 +0900

    no message

--- scripts/prepair.sh
+++ scripts/prepair.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export composer="/usr/local/bin/composer"
-export PATH=/usr/local/bin/composer:$PATH
+export composer="/home/ec2-user/composer"
+export PATH=/home/ec2-user/composer:$PATH
 source ~/.bashrc
 source ~/.bash_profile
-cd /var/www/html/okura-restful-api
+cd /var/www/html/okura-restful-api/
 composer update
