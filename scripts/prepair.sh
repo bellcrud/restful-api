a817a34278ce5a4f11cb5ba732a27787bec16046
okurashoichi <okurashoichi@team-lab.com>
2019-06-30 04:32:51 +0900

    no message

--- scripts/prepair.sh
+++ scripts/prepair.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-sudo rm -r /var/www/html/okura-restful-api/storage/logs/
-sudo mkdir /var/www/html/okura-restful-api/storage/logs/
-sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-sudo /sbin/mkswap /var/swap.1
-sudo /sbin/swapon /var/swap.1
+sudo php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+
 cd /var/www/html/okura-restful-api/
 /usr/local/bin/composer update
