38883e6b6863fbba1dfbb3f28a40cdf6348a32b0
okurashoichi <okurashoichi@team-lab.com>
2019-06-30 06:47:28 +0900

    no message

--- scripts/prepair.sh
+++ scripts/prepair.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
 cd /var/www/html/okura-restful-api/
 /usr/local/bin/composer update
