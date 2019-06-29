239a519afdca1a828e0bcb82942102a8af872d45
okurashoichi <okurashoichi@team-lab.com>
2019-06-30 02:55:17 +0900

    no message

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo rm -r /var/www/html/okura-restful-api/storage/logs
 sudo rm -r /var/www/html/okura-restful-api/storage/framework
+sudo rm -r /var/www/html/okura-restful-api/bootstrap/cache
 sudo mkdir /var/www/html/okura-restful-api/storage/logs
 sudo mkdir /var/www/html/okura-restful-api/storage/framework
+sudo mkdir /var/www/html/okura-restful-api/bootstrap/cache
