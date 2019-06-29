991a311988f3ba0ef407d7f28b4abd22b4e9997f
okurashoichi <okurashoichi@team-lab.com>
2019-06-30 02:07:28 +0900

    no message

--- scripts/prepair.sh
+++ scripts/prepair.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo rm -r /var/www/html/okura-restful-api/storage/logs/
+sudo mkdir /var/www/html/okura-restful-api/storage/logs/
 sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 sudo /sbin/mkswap /var/swap.1
 sudo /sbin/swapon /var/swap.1
