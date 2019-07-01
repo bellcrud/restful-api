32f72ca10448e5bf1622ee643627cde233315a6b
okurashoichi <okurashoichi@team-lab.com>
2019-07-01 20:49:36 +0900

    no message

--- codedeploy_deploy.py
+++ codedeploy_deploy.py
@@ -19,7 +19,7 @@ import sys
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_KEY = 'okura-app-1a/app-bitbucket_builds.zip'
+BUCKET_KEY = 'app-bitbucket_builds.zip'
 
 
 def upload_to_s3(artifact):
